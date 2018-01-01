096ebc876309a8a8c7f112e3eed6bbfa8e0f24a3
OldDriver-Lee <a0972186646@gmail.com>
2018-01-01 23:22:35 +0800

    106360213

--- source/main.cpp
+++ source/main.cpp
@@ -57,3 +57,51 @@ slotMachine::slotMachine()//先把上面定義的private的值設定好
 	wheelC = 0;
 	gameCost = 2;
 }
+
+double slotMachine::calculatePayout()//如果有用到3個一樣的數字，會得到金額
+{
+	if (wheelA == wheelB  && wheelA == wheelC)
+	{
+		switch (wheelA)
+		{
+		case 1:
+		case 3:
+		case 5://3個1 or 3個3 or 3個5 你會得到機台裡面的金額*0.2 
+			payOut = ((moneyInMachine + 1) * 0.2);
+			moneyInMachine -= payOut;
+			moneyInPeople += payOut;
+			cout << "You win!" << endl;
+			cout << "Pulls Left: " << pullTimes << endl;
+			return payOut;
+			break;
+		case 2:
+		case 4:
+		case 6://3個2 or 3個4 or 3個6 你會得到機台裡面的金額*0.4 
+			payOut = (moneyInMachine * 0.4);
+			moneyInMachine -= payOut;
+			moneyInPeople += payOut;
+			cout << "You win!" << endl;
+			cout << "Pulls Left: " << pullTimes << endl;
+			return payOut;
+			break;
+		case 7://3個7 中大獎 機台裡面的錢全拿
+			payOut = (moneyInMachine);
+			pullTimes = 0;
+			moneyInMachine = 0;
+			moneyInPeople += payOut;
+			cout << "You win!" << endl;
+			cout << "Pulls Left: " << pullTimes << endl;
+			return payOut;
+			break;
+		default:
+			return 0;
+			break;
+		}
+	}
+	else//都沒有3個一樣的
+	{
+		cout << "Pulls Left: " << pullTimes << endl;
+		cout << "No payout. Try again?\n" << endl;
+		return 0;
+	}
+}
